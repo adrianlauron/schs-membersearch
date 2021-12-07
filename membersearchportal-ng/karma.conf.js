bf344cd76b464777913249384a3dc04c0f8d62a4
Adrian Lauron <adrian.lauron@hotmail.com>
2021-12-07 22:33:44 +1300

    added gitignore and updated app name

--- membersearchportal-ng/karma.conf.js
+++ membersearchportal-ng/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/fe-ng'),
+      dir: require('path').join(__dirname, './coverage/membersearch-portal'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- membersearchportal-ng/src/app/app.component.spec.ts
+++ membersearchportal-ng/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fe-ng'`, () => {
+  it(`should have as title 'membersearch-portal'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fe-ng');
+    expect(app.title).toEqual('membersearch-portal');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fe-ng app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('membersearch-portal app is running!');
   });
 });
